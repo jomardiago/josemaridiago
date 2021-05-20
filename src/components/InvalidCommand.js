8d18048efcc30a5f6abc9eee2184f554c7c6567e
Jose-Diago <jose.diago@ibm.com>
2021-05-20 10:28:51 +0800

    Adjusting margin for invalid commands.

--- src/components/InvalidCommand.js
+++ src/components/InvalidCommand.js
@@ -4,10 +4,10 @@ function InvalidCommand() {
     return (
         <div>
             <p className="red-text">You may have accidentally entered an invalid command. No worries, please see commands below.</p>
-            <p><span className="yellow-text">jad --info</span> : Shows my personal information I guess?</p>
-            <p><span className="yellow-text">jad --skills</span> : Shows my skills, how about that! Hopefully impostor syndrome won't kick in!</p>
-            <p><span className="yellow-text">jad --connect</span> : I don't know, maybe connect with me?</p>
-            <p><span className="yellow-text">jad --clear</span> : Clear all the commands you have type.</p>
+            <p className="m-0"><span className="yellow-text">jad --info</span> : Shows my personal information I guess?</p>
+            <p className="m-0"><span className="yellow-text">jad --skills</span> : Shows my skills, how about that! Hopefully impostor syndrome won't kick in!</p>
+            <p className="m-0"><span className="yellow-text">jad --connect</span> : I don't know, maybe connect with me?</p>
+            <p className="m-0"><span className="yellow-text">jad --clear</span> : Clear all the commands you have type.</p>
         </div>
     );
 };
