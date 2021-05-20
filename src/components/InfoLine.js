744c61062720fbcb763967ca9fbd0aaab4c54068
Jose-Diago <jose.diago@ibm.com>
2021-05-20 10:25:21 +0800

    Adjusting some margins.

--- src/components/InfoLine.js
+++ src/components/InfoLine.js
@@ -18,10 +18,10 @@ function InfoLine({ basic }) {
                         <p>
                             <span>Showing available commands. Just type <span className="green-text">'jad --{'{command}'}'</span> to run the command. I.E. <span className="yellow-text">jad --info</span></span>
                         </p>
-                        <p><span className="yellow-text">jad --info</span> : Shows my personal information I guess?</p>
-                        <p><span className="yellow-text">jad --skills</span> : Shows my skills, how about that! Hopefully impostor syndrome won't kick in!</p>
-                        <p><span className="yellow-text">jad --connect</span> : I don't know, maybe connect with me?</p>
-                        <p><span className="yellow-text">jad --clear</span> : Clear all the commands you have type.</p>
+                        <p className="m-0"><span className="yellow-text">jad --info</span> : Shows my personal information I guess?</p>
+                        <p className="m-0"><span className="yellow-text">jad --skills</span> : Shows my skills, how about that! Hopefully impostor syndrome won't kick in!</p>
+                        <p className="m-0"><span className="yellow-text">jad --connect</span> : I don't know, maybe connect with me?</p>
+                        <p className="m-0"><span className="yellow-text">jad --clear</span> : Clear all the commands you have type.</p>
                     </>
                 )
             }
