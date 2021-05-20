380fe0cb92516258cff2889fb4bd6445b3c309ed
Jose-Diago <jose.diago@ibm.com>
2021-05-20 19:39:40 +0800

    Adding period, I need to stop doing this.

--- src/components/CommandsList.js
+++ src/components/CommandsList.js
@@ -4,7 +4,7 @@ function CommandsList() {
     return (
         <div>
             <p className="m-0"><span className="yellow-text">jad --info</span> : Shows my personal information I guess?</p>
-            <p className="m-0"><span className="yellow-text">jad --stack</span> : A day in a life of my technolgy stack</p>
+            <p className="m-0"><span className="yellow-text">jad --stack</span> : A day in a life of my technolgy stack.</p>
             <p className="m-0"><span className="yellow-text">jad --connect</span> : I don't know, maybe connect with me?</p>
             <p className="m-0"><span className="yellow-text">jad --clear</span> : Clear all the commands you have type.</p>
         </div>
